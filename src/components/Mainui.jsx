5924f6451cdb27b76229457e5a1a8178f2b1fad4
rvishakhs <rvishakhs@gmail.com>
2023-03-04 00:37:15 +0000

    Side bar completed

--- src/components/Mainui.jsx
+++ src/components/Mainui.jsx
@@ -5,11 +5,19 @@ import {
 
 } from '@ant-design/icons';
 import {BsPeople} from "react-icons/bs"
-import {AiOutlineDashboard} from "react-icons/ai"
-import {CgFolderAdd, CgFolder } from "react-icons/cg"
+import {AiOutlineDashboard, AiOutlineFileSearch} from "react-icons/ai"
+import {CgFolderAdd, CgFolder, CgDuplicate } from "react-icons/cg"
 import {BiCategoryAlt} from "react-icons/bi"
+import {ImBooks, ImProfile} from "react-icons/im"
+import {GiNotebook} from "react-icons/gi"
+import {FiLogOut} from "react-icons/fi"
+import {TbFileInvoice, TbNotebook} from "react-icons/tb"
+import {MdCategory, MdManageAccounts} from "react-icons/md"
+import {RiLockPasswordLine} from "react-icons/ri"
+
 import { Layout, Menu, theme } from 'antd';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const { Header, Sider, Content } = Layout;
 
 
@@ -18,15 +26,31 @@ const Mainui = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const navigate = useNavigate()
   return (
-    <Layout className='h-screen'>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo py-2" />
-        <p></p>
+    <Layout className='h-screen '>
+      <Sider 
+        className=''  
+        trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo " />
+        <div className='w-full flex items-center justify-center bg-yellow-400 py-2 px-2 mb-3'>
+          {
+            collapsed?  (<p className='font-semibold text-xl transition !delay-700 !duration-700 ease-linear '>ES</p>)  : ( <p className='font-semibold text-xl transition !delay-700 !duration-700 ease-linear'>EShoppers</p>)
+          }
+        </div>
         <Menu
           theme="dark"
           mode="inline"
+          className=''
           defaultSelectedKeys={['dashbard']}
+          onClick={({key}) => {
+            if(key === "signout ") {
+
+            } else {
+              navigate(key)
+            }
+          }} 
           items={[
             {
               key: 'dashbard',
@@ -53,7 +77,66 @@ const Mainui = () => {
                 icon: <CgFolderAdd className='w-5 h-5' />,
                 label: 'Add-products',
               },
+                {
+                key: 'category',
+                icon: <MdCategory className='w-5 h-5' />,
+                label: 'Category',
+              },
+                {
+                key: 'addcategory',
+                icon: <CgDuplicate className='w-5 h-5' />,
+                label: 'Add-Category',
+              },
             ]
+            },
+            {
+              key: 'orders',
+              icon: <TbFileInvoice className='w-5 h-5' />,
+              label: 'Orders',
+            },
+            {
+              key: 'blog',
+              icon: <ImBooks className='w-5 h-5' />,
+              label: 'Blog',
+              children: [
+                {
+                  key: 'blogs',
+                  icon: <TbNotebook className='w-5 h-5' />,
+                  label: 'Blogs',
+                },
+                {
+                  key: 'addblogs',
+                  icon: <GiNotebook className='w-5 h-5' />,
+                  label: 'Add-Blogs',
+                }
+              ]
+            },
+            {
+              key: 'enquiry',
+              icon: <AiOutlineFileSearch className='w-5 h-5' />,
+              label: 'Enquiry',
+            },
+            {
+              key: 'account',
+              icon: <MdManageAccounts className='w-5 h-5' />,
+              label: 'Account',
+              children: [
+                {
+                  key: 'profile',
+                  icon: <ImProfile className='w-5 h-5' />,
+                  label: 'Profile',
+                },
+                {
+                  key: 'resetpassword',
+                  icon: <RiLockPasswordLine className='w-5 h-5' />,
+                  label: 'Reset Password',
+                },
+                {
+                  key: 'logout',
+                  icon: <FiLogOut className='w-5 h-5' />,
+                  label: 'Logout',
+                }
+              ]
             },
            
           ]}
