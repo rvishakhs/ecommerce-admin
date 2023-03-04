0c30ead560d3f11884bcefdfaa89ea2ff4652a06
rvishakhs <rvishakhs@gmail.com>
2023-03-04 20:19:59 +0000

    working on header sections

--- src/components/Mainui.jsx
+++ src/components/Mainui.jsx
@@ -10,6 +10,7 @@ import {CgFolderAdd, CgFolder, CgDuplicate } from "react-icons/cg"
 import {BiCategoryAlt} from "react-icons/bi"
 import {ImBooks, ImProfile} from "react-icons/im"
 import {GiNotebook} from "react-icons/gi"
+import {BsBell} from "react-icons/bs"
 import {FiLogOut} from "react-icons/fi"
 import {TbFileInvoice, TbNotebook} from "react-icons/tb"
 import {MdCategory, MdManageAccounts} from "react-icons/md"
@@ -34,7 +35,7 @@ const Mainui = () => {
         className=''  
         trigger={null} collapsible collapsed={collapsed}>
         <div className="logo " />
-        <div className='w-full flex items-center justify-center bg-yellow-400 py-2 px-2 mb-3'>
+        <div className='w-full flex items-center justify-center bg-yellow-400 py-3 px-2 mb-3'>
           {
             collapsed?  (<p className='font-semibold text-xl transition !delay-700 !duration-700 ease-linear '>ES</p>)  : ( <p className='font-semibold text-xl transition !delay-700 !duration-700 ease-linear'>EShoppers</p>)
           }
@@ -148,12 +149,32 @@ const Mainui = () => {
             padding: 0,
             background: colorBgContainer,
           }}
-          className="px-2"
+          className="px-2 "
         >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: () => setCollapsed(!collapsed),
-          })}
+          <div className='flex justify-between px-2'>
+            <div className='px-2 ml-2 text-2xl mt-2'>
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger ',
+                onClick: () => setCollapsed(!collapsed),
+              })}
+            </div>
+            <div className='flex '>
+                <div className='flex relative items-center gap-2'>
+                    <BsBell className=' w-6 mt-2 h-6 mr-4 cursor-pointer ' />
+                    <div className='absolute w-3 top-[12px] right-[12px] animate-pulse h-3 rounded-full bg-yellow-400'></div>
+                </div>
+                <div className='flex items-center space-x-1'>
+                  <img 
+                    src='https://avatars.githubusercontent.com/u/56556101?v=4'
+                    className='w-9 h-10 mt-2 rounded-xl object-contain'
+                  />
+                  <div className='flex flex-col mt-2 px-2'>
+                    <p className='font-medium text-lg text-black tracking-wide'>Visakh SR</p>
+                    <p className='font-medium text-base text-gray-500 '>Rvishaks@gmail.com</p>
+                  </div>
+                </div>
+            </div>
+          </div>
         </Header>
         <Content
           style={{
