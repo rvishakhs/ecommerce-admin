4507883a75070919928d1e4be169a7f1e0aee3a7
rvishakhs <rvishakhs@gmail.com>
2023-04-12 19:44:32 +0530

    image upload function restructured

--- src/pages/ColorList.jsx
+++ src/pages/ColorList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import {  Table } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import { getblogs } from '../features/blogs/blogSlice';
 import { Link } from 'react-router-dom';
 import {FiEdit} from 'react-icons/fi'
 import {AiOutlineDelete} from 'react-icons/ai'
